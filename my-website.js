b4c12afb8278e06441c276d6475cc0d6be7e94ea
Joafie <jade424433@gmail.com>
2018-07-31 18:44:34 -0700

    changed app.js filename to my-website.js

--- my-website.js
+++ my-website.js
@@ -6,5 +6,4 @@ app.use(express.json());
 app.use('/api', api);
 app.use(express.static('client/static', {extensions: ['html']}));
 
-
 app.listen(3000, () => console.log('Listening on port 3000...'));
