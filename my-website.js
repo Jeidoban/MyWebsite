0230f7135d712f96475fc31c35310b0980980fc0
Joafie <jade424433@gmail.com>
2018-08-02 14:45:36 -0700

    Changed directory structure

--- my-website.js
+++ my-website.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const api = require('./routes/api');
+const path = require('path');
 
 app.use(express.json());
 app.use('/api', api);
-app.use(express.static('client/static', {extensions: ['html']}));
+app.use(express.static(path.join(__dirname, '/client/static')/*, {extensions: ['html']}*/));
+//app.use('/projects/bank-ledger', express.static(path.join(__dirname, '/client/apps/bank-ledger/build')));
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
